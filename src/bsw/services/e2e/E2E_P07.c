96279c49291e771008854e5afe13e9ffb3452e92
YuleTech Developer <dev@yuletech.com>
2026-05-03 08:30:19 +0800

    补充文件头注释：为剩余文件添加标准头
    - Det_Stub.c/h: 添加DET stub模块说明
    - E2E_P07.c: 添加E2E Profile 7模块说明
    
    完成代码评阅报告中关于文件头注释的建议项

--- src/bsw/services/e2e/E2E_P07.c
+++ src/bsw/services/e2e/E2E_P07.c
@@ -1,3 +1,16 @@
-/* E2E Profile 7 - CRC32 with flexible length */
+/**
+ * @file E2E_P07.c
+ * @brief E2E Profile 7 - CRC32 with flexible data length
+ * @copyright Copyright (c) 2025 yuleASR Project
+ * @license MIT License
+ * 
+ * AUTOSAR Classic Platform - End-to-End Protection
+ * Profile 7 implementation optimized for flexible data length.
+ * Uses CRC32 checksum for data integrity protection.
+ * 
+ * @see AUTOSAR Specification of E2E Protocol
+ */
+
 #include "E2E.h"
+
 /* Profile 7 implementation - optimized for flexible data length */
