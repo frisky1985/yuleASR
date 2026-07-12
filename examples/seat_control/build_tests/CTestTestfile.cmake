666d17e45b628ac13fdf2bab89f160476dd42df0
Stefan Ming <stefan@yuletech.com>
2026-07-12 23:09:41 +0800

    chore: gitignore build_tests for seat_control demo

--- examples/seat_control/build_tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/stefan/.openclaw/workspace/yuleASR/examples/seat_control
-# Build directory: /Users/stefan/.openclaw/workspace/yuleASR/examples/seat_control/build_tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
